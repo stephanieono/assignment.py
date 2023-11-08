325fd5d5089ddfac7e34fd2c787c991b80ac7187
stephanieono <biancastephanie2k23@gmail.com>
2023-11-08 17:47:49 +0100

    my assignment

---
+++ main.py
@@ -0,0 +1,9 @@
+business_name='yusuf & sons'
+print(business_name)
+principal = float(input('Enter amount: '))
+time = float(input('Enter time: '))
+rate = float(input('Enter rate: '))
+simple_interest=(principal*time*rate)/100
+compound_interest=principal*( (1+rate/100)**time-1)
+print('simple interest is : %f' %(simple_interest))
+print('compound interest is : %f' %(compound_interest))
